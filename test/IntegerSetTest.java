26c1de535f7064d741cd25bc98fa6ddf25d91cc6
Ivan Boatwright <ijboatwright@gmail.com>
2019-05-08 04:38:48 -0400

    wip reading test_input into the needed lists

--- test/IntegerSetTest.java
+++ test/IntegerSetTest.java
@@ -5,58 +5,111 @@
  * Assignment: 6
  * Date: 5.7.19
  *
- * JUnit5 IntegerSet Parameterized Unit Tests
+ * JUnit5.4.2 IntegerSet Parameterized Unit Tests
  */
 
-import IntegerSet;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class IntegerSetTest {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class IntegerSetTest {
+  List<List<Integer>> testData;
+
+  IntegerSet{
+    Scanner test_input;
+
+    // open the test_input file
+    try {
+      test_input = new Scanner(new File("test_input.txt"));
+    } catch(FileNotFoundException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    // read each line and split it into an Integer list
+    while (test_input.hasNextLine()) {
+      testData.add(
+          Stream
+              .of(test_input.nextLine().split(","))
+              .map(Integer::valueOf)
+              .collect(Collectors.toList())
+      );
+    }
+
+    // TODO: split testData into the 6 lists
+
+  }
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
 
   // tests null inputs for intersection
-  public void testIntersectionWithNullInput(){
+  @Test
+  void testIntersectionWithNullInput(){
 
   }
 
   // tests if  a value exists in a set
-  public void testExists(){}
+  @ParameterizedTest
+  void testExists(){}
 
 
   // tests if an IntegerSet is empty
-  public void testIsEmpty(){}
+  @Test
+  void testIsEmpty(){}
 
 
   // tests the union of two sets
-  public void testUnion(){}
+  @Test
+  void testUnion(){}
 
 
   // tests the creation of a set from an array
-  public void testCreateSetFromArray(){}
+  @Test
+  void testCreateSetFromArray(){}
 
 
   // tests the creation of a set from  null
-  public void testCreateSetFromNull(){}
+  @Test
+  void testCreateSetFromNull(){}
 
 
-  public void testDeleteAll(){}
+  @Test
+  void testDeleteAll(){}
 
 
-  public void testDeleteElement(){}
+  @Test
+  void testDeleteElement(){}
 
 
-  public void testInsertAll(){}
+  @Test
+  void testInsertAll(){}
 
 
-  public void testAllNull(){}
+  @Test
+  void testAllNull(){}
 
 
-  public void testIntersection(){}
+  @Test
+  void testIntersection(){}
 
 
-  public void testUnionWithNullInput(){}
+  @Test
+  void testUnionWithNullInput(){}
 
 }
