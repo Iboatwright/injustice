8ea630dcbe930114e145498279a7f40e362e3d53
Ivan Boatwright <ijboatwright@gmail.com>
2019-05-07 21:44:36 -0400

    added some test imports

---
+++ test/IntegerSetTest.java
@@ -0,0 +1,62 @@
+/**
+ * File: IntegerSetTest.java
+ * Author: 5153
+ * Course: CEN 4072
+ * Assignment: 6
+ * Date: 5.7.19
+ *
+ * JUnit5 IntegerSet Parameterized Unit Tests
+ */
+
+import IntegerSet;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class IntegerSetTest {
+
+  // tests null inputs for intersection
+  public void testIntersectionWithNullInput(){
+
+  }
+
+  // tests if  a value exists in a set
+  public void testExists(){}
+
+
+  // tests if an IntegerSet is empty
+  public void testIsEmpty(){}
+
+
+  // tests the union of two sets
+  public void testUnion(){}
+
+
+  // tests the creation of a set from an array
+  public void testCreateSetFromArray(){}
+
+
+  // tests the creation of a set from  null
+  public void testCreateSetFromNull(){}
+
+
+  public void testDeleteAll(){}
+
+
+  public void testDeleteElement(){}
+
+
+  public void testInsertAll(){}
+
+
+  public void testAllNull(){}
+
+
+  public void testIntersection(){}
+
+
+  public void testUnionWithNullInput(){}
+
+}
