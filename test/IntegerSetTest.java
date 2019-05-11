6353686efbcc2c4f469acc3a0b31df4bd3c50140
Ivan Boatwright <ijboatwright@gmail.com>
2019-05-11 16:37:05 -0400

    fixed a bug where test_set only held the last set. added helper vars to improve readability. this version converts the input sets to List<Integer>s, but IntegerSet expects Integer[] arrays. TODO covert stream to output Integer[] arrays.

--- test/IntegerSetTest.java
+++ test/IntegerSetTest.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,7 +32,7 @@ import java.util.stream.Stream;
 @RunWith(Parameterized.class)
 class IntegerSetTest {
   private IntegerSet integerSet;
-  private int[] first_list_of_numbers;
+  private List<Integer> first_list_of_numbers;
   private int[] unique_numbers_in_the_first_list;
   private int[] second_list_of_numbers;
   private int[] unique_numbers_in_the_second_list;
@@ -41,7 +40,7 @@ class IntegerSetTest {
   private int[] union_of_set1_and_set2;
 
   // Constructor for assigning current test set to instance variables
-  public IntegerSetTest(int[] first, int[] first_unique, int[] second,
+  public IntegerSetTest(List<Integer> first, int[] first_unique, int[] second,
                         int[] second_unique, int[] intersection, int[] union){
     this.first_list_of_numbers = first;
     this.unique_numbers_in_the_first_list = first_unique;
@@ -54,23 +53,28 @@ class IntegerSetTest {
   @Parameterized.Parameters
   public static Collection<Object[]> data() {
     List<String> test_input = new ArrayList<>();
-    ArrayList<Object[]> testData = new ArrayList<>();
 
+    // read each line into an ordered List
     try {
       test_input = Files.readAllLines(Paths.get("test_input.txt"));
     } catch (IOException|SecurityException e) {
       e.printStackTrace();
     }
 
+    ArrayList<Object[]> testData = new ArrayList<>();
+    int inputsPerSet = 6;
+    int inputSets = test_input.size()/(inputsPerSet +1); // +1 delimiter line
+
     // loop through the test_input 7 lines at a time
     int line = 0;
-    Object[] test_set = new Object[6];
-    for (int i=0; i < test_input.size()/test_set.length; i++){
-      // copy the 6 lists of test data to test_set j as int arrays
-      for (int j=0; j<6; j++) {
-        test_set[j] = Stream.of(test_input.get(line++).split(","))
+    Object[][] test_set = new Object[inputSets][inputsPerSet];
+    for (int i=0; i < inputSets; i++){
+      // copy the 6 lists of test data to test_set j as boxed Integer Lists
+      for (int j=0; j < inputsPerSet; j++) {
+        test_set[i][j] = Stream.of(test_input.get(line++).split(","))
                             .mapToInt(Integer::parseInt)
-                            .toArray();
+                            .boxed()
+                            .collect(Collectors.toList());
       }
       // TEST - shows the output is an int[]
       //System.out.println(Stream.of("1,2,3,4".split(",")).mapToInt(Integer::parseInt).toArray()[i]);
@@ -83,7 +87,7 @@ class IntegerSetTest {
 
   @Before
   void setUp() {
-    integerSet = new IntegerSet();
+    integerSet = new IntegerSet(first_list_of_numbers);
   }
 
   @After
@@ -103,7 +107,6 @@ class IntegerSetTest {
     System.out.println("intersection: " + intersection_of_set1_and_set2[0]);
     assertNull(null);
   }
-
 
   // tests if an IntegerSet is empty
   @Test
